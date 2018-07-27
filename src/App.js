3aa1e728dd7ee92278b12d9f17d806df952a6f7f
smkim89 <kiak357@gmail.com>
2018-07-28 00:33:04 +0900

    setup apollo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import client from './ApolloClient';
+import { ApolloProvider } from 'react-apollo';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <ApolloProvider client={client} >
+        <div className="App" />
+      </ApolloProvider>
+      
     );
   }
 }
